40c41a64175813b75dbc142cfc988ff3e40f9749
Govind-Asawa <48401781+Govind-Asawa@users.noreply.github.com>
2024-05-06 10:17:17 -0500

    Create 283. Move Zeroes

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/move-zeroes/description/
+# two pointer approach
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right, n = 0, 0, len(nums)
+        nums += [0] #adding this just to avoid out-of-bounds on line 17. can be any dummy value
+        while right < n:
+            while left < n and nums[left] != 0:
+                left += 1
+            while right < n and nums[right] == 0:
+                right += 1
+            
+            if right <= left:
+                right = left + 1
+            else:
+                nums[left], nums[right] = nums[right], nums[left]
+        nums.pop()
