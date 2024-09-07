cce3acf90bb56b012f348fd778d62b7653f344d2
Govind-Asawa <48401781+Govind-Asawa@users.noreply.github.com>
2024-09-07 13:57:38 -0500

    Update 80. Remove Duplicates from Sorted Array II.py

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -18,3 +18,23 @@ class Solution:
             right += 1
 
         return left+1
+
+# generic approach, allows us to choose number of copies we want to retain
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        p = 0
+        copiesAllowed = 2
+        count = 1
+        for i in range(1,len(nums)):
+            if nums[i] == nums[p]:
+                count += 1
+                if count <= copiesAllowed:
+                    p += 1
+                    nums[p] = nums[i]
+            else: #encountered unique
+                p += 1
+                nums[p] = nums[i]
+                count = 1
+
+        return p+1
+                 
